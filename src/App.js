e895d0b81203d0ede4ec8d408725b6816687b061
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-01-24 02:37:53 +0530

    added basic firebase setup & HTML, CSS Sign up page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{minHeight:"70vh", display:"flex", justifyContent:"center", alignItems:"center", lineHeight:"100px"}}>
+         <form>
+        <input style={{width:"400px", fontSize:"25px"}} type = "text"  placeholder='userId'/> <br/>
+        <input style={{width:"400px", fontSize:"25px"}} type ="password" placeholder="password" /> <br></br>
+        <button  style={{width:"400px", fontSize:"25px" , backgroundColor:"#0095F6", color:"white", border:"none", padding:"5px 0", borderRadius:"10px"}}>Sign In</button>
+        </form>
+      </div>
+       <hr style={{width:"450px"}}></hr>
+      <h3>Have an account?</h3> 
+      <h2 style={{color:"#0095F6"}}>Log In</h2>
+      
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-XsJAVPyQcH4fhB2Zbj9ycG_gePJugJ8",
+  authDomain: "fir-ed981.firebaseapp.com",
+  projectId: "fir-ed981",
+  storageBucket: "fir-ed981.appspot.com",
+  messagingSenderId: "54657696364",
+  appId: "1:54657696364:web:f920bd1e69f0237d4cce49"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = app.auth();
+export default app;
